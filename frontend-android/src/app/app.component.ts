9a66087ea7fb20bde0704295add50783a54d85e8
Kenneth Van Gysegem <kenneth.vangysegem@gmail.com>
2026-03-24 18:43:36 +0100

    android: handle back button to navigate back or exit app

--- frontend-android/src/app/app.component.ts
+++ frontend-android/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Location } from '@angular/common';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private location: Location) {}
+
+  ngOnInit() {
+    App.addListener('backButton', () => {
+      if (window.history.length > 1) {
+        this.location.back();
+      } else {
+        App.exitApp();
+      }
+    });
+  }
+}
